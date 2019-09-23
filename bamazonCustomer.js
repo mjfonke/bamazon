4b0dbf3dbd5b0c0b9ce38d3aec456ed34bdbe359
Miran Jang <miranjang9507@gmail.com>
2019-09-23 10:35:20 -0400

    fixed bug

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,7 +18,6 @@ connection.connect(function(err) {
 function list() {
     var query = "select * from product";
     connection.query(query, function(err, res) {
-        console.log("Working?")
         var table = new Table({
             head: ["ITEM_ID", "PRODUCT_NAME", "DEPARTMENT_NAME", "PRICE", "STOCK_QUANTITY"],
         });
@@ -28,32 +27,74 @@ function list() {
        );
         };
         console.log(table.toString());
+   
+    inquirer.prompt([
+        {
+        name: "id",
+        type: "input",
+        message: "What is the ID of the item you would like to purchase? [Quit with Q]",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            return false;
+        }
+        },
 
-        buy();
+        {
+            name: "units",
+            type: "input",
+            message: "How many would you like to purchase?[Q for Quit]",
+            validate: function(value) {
+                if (isNaN(value) === false) {   
+                 return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function(answer) {
+            var arrNum = parseInt(answer.id - 1);
+            var product = answer.id;
+            var quantity = parseInt(answer.units);
+            var totalPrice = parseFloat((res[arrNum].price * quantity));
+
+            if (quantity > res[arrNum].stock_quantity) {
+                console.log("Sorry! We do not have enough item in our stock. Please check our stock_quantity again!");
+                prompt();
+            }
+
+            else {var query = "UPDATE product set ? where ?";
+            connection.query(query, [{stock_quantity: (res[arrNum].stock_quantity - quantity)}, {item_id: product}], function(err, res) {
+                
+                if(err) throw err;
+            console.log("Your total price is $" + totalPrice + ". Thank you for your purchase!")
+
+            prompt();
+            
+            });
+            }
+    });
 
     });
     
 };
 
-function buy() {
-    inquirer.prompt({
-        name: "id",
-        type: "input",
-        message: "What is the ID of the item you would like to purchase? [Quit with Q]"
-    }).then(function(answer) {
-        if (answer.id === "q") {
-            console.log("Good-Bye!");
+
+function prompt() {
+
+    inquirer.prompt(
+        {
+            name: "yesno",
+            type: "list",
+            message: "Would you like to purchase another items?",
+            choices: ["YES", "NO"]
+        }
+    ).then(function(answer) {
+        if (answer.yesno === "YES") {
+            list();
+        } else {
+            console.log("Good Bye!")
             connection.end();
         }
-        if (answer.id === NaN) {
-            console.log("Please Enter ID number you would like to purchase");
-            return;
-        } 
-        // else {
-
-        // }
     });
-
-
-
 };
